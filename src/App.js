59b4e4c7705441743fabd5b143ed9bf72cf4d925
Lucas-Ballonje <lucas.ballonje@ativo.com>
2022-02-09 15:50:34 -0300

    API funcionando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from "@apollo/client";
+import INFO_PERSON from "./query";
+import  "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function app(){
+  const { loading, error, data } = useQuery(INFO_PERSON);
+
+  if(loading){
+    return <p>Loading...</p>
+  }
+
+  if(error){
+    <p>an error occured...</p>
+  }
+
+  return(
+    <section className="parent">
+      {data.characters.results.map((person, index) => (
+        <div className="card" kay={person.name}>
+          <img src={person.image} alt="Avatar" style={{width: "100%"}}/>
+          <div className="container">
+              <h4><b>{person.name}</b></h4>
+
+              <p><b>GENDER:</b></p> {person.gender}
+              <p><b>SPECIES:</b></p> {person.species}
+              
+          </div>
+        </div>
+      ))}
+    </section>
+  )
 }
 
 export default App;
